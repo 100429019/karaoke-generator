93620f387d3b2389a8c265e9c4a4ebdae8ea92d3
rafam <100429019@alumnos.uc3m.es>
2023-02-18 18:45:40 +0100

    Transcripción con Whisper

--- generate.py
+++ generate.py
@@ -1,22 +1,27 @@
-import speech_recognition as sr
+#import speech_recognition as sr
+import whisper
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, AudioFileClip
 import os
-
+import ffmpeg
+print(ffmpeg.__file__)
 PATH = os.getcwd() + '/test/'
 
 for instance in os.listdir(PATH):
     # Load the video clip and audio clip
     video_clip = VideoFileClip(PATH + instance + "/video.mp4")
     audio_clip = AudioFileClip(PATH + instance + "/instrumental.wav")
-
-    # Set up the speech recognizer and load the audio file
-    r = sr.Recognizer()
-    with sr.AudioFile(PATH + instance + "/vocals.wav") as source:
-        audio = r.record(source)
-
-    # Transcribe the audio file using CMU Sphinx
-    transcription = r.recognize_sphinx(audio)
-
+    video_clip = video_clip.set_audio(audio_clip)
+    """    # Set up the speech recognizer and load the audio file
+        r = sr.Recognizer()
+        with sr.AudioFile(PATH + instance + "/vocals.wav") as source:
+            audio = r.record(source)
+    
+        # Transcribe the audio file using CMU Sphinx
+        transcription = r.recognize_sphinx(audio)
+    """
+    model = whisper.load_model("medium")
+    transcription = model.transcribe(PATH + instance + "/vocals.wav")
+    print(transcription['text'])
     # Create a TextClip from the transcription
     subtitles = TextClip(transcription, fontsize=24, color='white', bg_color='black')
 
@@ -24,7 +29,7 @@ for instance in os.listdir(PATH):
     subtitles = subtitles.set_duration(audio_clip.duration)
 
     # Add the audio and subtitle tracks to the video clip
-    final_clip = CompositeVideoClip([video_clip, subtitles.set_pos(('center', 'bottom')), audio_clip])
+    final_clip = CompositeVideoClip([video_clip, subtitles.set_pos(('center', 'bottom'))])
 
     # Write the final clip to a file
     final_clip.write_videofile(PATH + instance + "/karaoke.mp4")
