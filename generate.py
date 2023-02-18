54ee24cb9b497fbb450a69d0d53bd38a366b2e9a
100429019 <100429019@alumnos.uc3m.es>
2023-02-18 15:55:56 +0100

    Funciona (mal - audio y subtitulos)

--- generate.py
+++ generate.py
@@ -8,23 +8,21 @@ for instance in os.listdir(PATH):
     # Load the video clip and audio clip
     video_clip = VideoFileClip(PATH + instance + "/video.mp4")
     audio_clip = AudioFileClip(PATH + instance + "/instrumental.wav")
-
+    video_clip.set_audio(audio_clip)
     # Set up the speech recognizer and load the audio file
     r = sr.Recognizer()
     with sr.AudioFile(PATH + instance + "/vocals.wav") as source:
         audio = r.record(source)
-
     # Transcribe the audio file using CMU Sphinx
     transcription = r.recognize_sphinx(audio)
-
     # Create a TextClip from the transcription
-    subtitles = TextClip(transcription, fontsize=24, color='white', bg_color='black')
+    subtitles = TextClip(transcription, fontsize=10, color='white', bg_color='black')
 
     # Set the duration of the TextClip to match the duration of the audio clip
     subtitles = subtitles.set_duration(audio_clip.duration)
 
     # Add the audio and subtitle tracks to the video clip
-    final_clip = CompositeVideoClip([video_clip, subtitles.set_pos(('center', 'bottom')), audio_clip])
+    final_clip = CompositeVideoClip([video_clip, subtitles.set_pos(('center', 'bottom'))])
 
     # Write the final clip to a file
     final_clip.write_videofile(PATH + instance + "/karaoke.mp4")
